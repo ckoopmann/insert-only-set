a9554c13eb8b670e6ccc5bbd74e227860ebbc7ce
christn <c.k.e.koopmann@gmail.com>
2024-05-10 12:39:56 +0800

    Add docs

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# insert-only-set
-Proc macro that generates an "InsertOnlySet" over the variants of an Enum using OnceLocks
+# Insert Only Set
+
+`insert_only_set` is a procedural macro to generates thread-safe insert-only sets from enums in Rust.
+Under the hood this set is a struct with a `OnceLock` field for each enum variant. 
+
+## Features
+
+- Automatically generates an insert-only set for any enum
+- Thread-safe insertions with `OnceLock`
+- Iterates over set variants that have been inserted
+
+## Example
+
+```rust 
+use insert_only_set::GenerateInsertOnlySet;
+
+#[derive(GenerateInsertOnlySet, Debug, PartialEq)]
+pub enum Type {
+    Customer,
+    Employee,
+}
+
+fn main() {
+    let set = TypeInsertOnlySet::new();
+
+    assert!(!set.contains(Type::Customer));
+    assert!(!set.contains(Type::Employee));
+
+    assert!(set.insert(Type::Customer));
+    assert!(set.contains(Type::Customer));
+    assert!(!set.contains(Type::Employee));
+
+    assert!(set.insert(Type::Employee));
+    assert!(set.contains(Type::Customer));
+    assert!(set.contains(Type::Employee));
+
+    // Try to insert again, should return false
+    assert!(!set.insert(Type::Customer));
+
+    for variant in set.iter() {
+        println!("{:?}", variant);
+    }
+}
+```
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,43 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Ident};
+use syn::{parse_macro_input, DeriveInput, Data, Ident};
 
+/// A procedural macro to generate an insert-only set for any enum.
+///
+/// This macro generates a struct with `insert`, `contains`, and `iter` methods for an enum.
+/// The struct uses `OnceLock` for thread-safe, one-time insertion of enum variants.
+///
+/// # Examples
+///
+/// ```rust
+/// use insert_only_set::GenerateInsertOnlySet;
+///
+/// #[derive(GenerateInsertOnlySet, Debug, PartialEq)]
+/// pub enum Type {
+///     Customer,
+///     Employee,
+/// }
+///
+/// fn main() {
+///     let set = TypeInsertOnlySet::new();
+///
+///     assert!(!set.contains(Type::Customer));
+///     assert!(!set.contains(Type::Employee));
+///
+///     set.insert(Type::Customer);
+///     assert!(set.contains(Type::Customer));
+///     assert!(!set.contains(Type::Employee));
+///
+///     set.insert(Type::Employee);
+///     assert!(set.contains(Type::Customer));
+///     assert!(set.contains(Type::Employee));
+///
+///     for variant in set.iter() {
+///         println!("{:?}", variant);
+///     }
+/// }
+/// ```
 #[proc_macro_derive(GenerateInsertOnlySet)]
 pub fn generate_add_only_set(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -12,102 +47,67 @@ pub fn generate_add_only_set(input: TokenStream) -> TokenStream {
     let set_name = Ident::new(&format!("{}InsertOnlySet", name), name.span());
 
     let fields = if let Data::Enum(ref data_enum) = input.data {
-        data_enum
-            .variants
-            .iter()
-            .map(|variant| {
-                let field_name = Ident::new(
-                    &variant.ident.to_string().to_lowercase(),
-                    variant.ident.span(),
-                );
-                quote! {
-                    pub #field_name: std::sync::OnceLock<bool>,
-                }
-            })
-            .collect::<Vec<_>>()
+        data_enum.variants.iter().map(|variant| {
+            let field_name = Ident::new(&variant.ident.to_string().to_lowercase(), variant.ident.span());
+            quote! {
+                pub #field_name: std::sync::OnceLock<bool>,
+            }
+        }).collect::<Vec<_>>()
     } else {
         vec![]
     };
 
     let new_fields_init = if let Data::Enum(ref data_enum) = input.data {
-        data_enum
-            .variants
-            .iter()
-            .map(|variant| {
-                let field_name = Ident::new(
-                    &variant.ident.to_string().to_lowercase(),
-                    variant.ident.span(),
-                );
-                quote! {
-                    #field_name: std::sync::OnceLock::new(),
-                }
-            })
-            .collect::<Vec<_>>()
+        data_enum.variants.iter().map(|variant| {
+            let field_name = Ident::new(&variant.ident.to_string().to_lowercase(), variant.ident.span());
+            quote! {
+                #field_name: std::sync::OnceLock::new(),
+            }
+        }).collect::<Vec<_>>()
     } else {
         vec![]
     };
 
     let insert_methods = if let Data::Enum(ref data_enum) = input.data {
-        data_enum
-            .variants
-            .iter()
-            .map(|variant| {
-                let field_name = Ident::new(
-                    &variant.ident.to_string().to_lowercase(),
-                    variant.ident.span(),
-                );
-                let variant_name = &variant.ident;
-                quote! {
-                    Type::#variant_name => {
-                        if self.#field_name.set(true).is_ok() {
-                            true
-                        } else {
-                            false
-                        }
-                    },
-                }
-            })
-            .collect::<Vec<_>>()
+        data_enum.variants.iter().map(|variant| {
+            let field_name = Ident::new(&variant.ident.to_string().to_lowercase(), variant.ident.span());
+            let variant_name = &variant.ident;
+            quote! {
+                Type::#variant_name => {
+                    if self.#field_name.set(true).is_ok() {
+                        true
+                    } else {
+                        false
+                    }
+                },
+            }
+        }).collect::<Vec<_>>()
     } else {
         vec![]
     };
 
     let contains_methods = if let Data::Enum(ref data_enum) = input.data {
-        data_enum
-            .variants
-            .iter()
-            .map(|variant| {
-                let field_name = Ident::new(
-                    &variant.ident.to_string().to_lowercase(),
-                    variant.ident.span(),
-                );
-                let variant_name = &variant.ident;
-                quote! {
-                    Type::#variant_name => self.#field_name.get().copied().unwrap_or(false),
-                }
-            })
-            .collect::<Vec<_>>()
+        data_enum.variants.iter().map(|variant| {
+            let field_name = Ident::new(&variant.ident.to_string().to_lowercase(), variant.ident.span());
+            let variant_name = &variant.ident;
+            quote! {
+                Type::#variant_name => self.#field_name.get().copied().unwrap_or(false),
+            }
+        }).collect::<Vec<_>>()
     } else {
         vec![]
     };
 
     let iter_body = if let Data::Enum(ref data_enum) = input.data {
-        data_enum
-            .variants
-            .iter()
-            .map(|variant| {
-                let field_name = Ident::new(
-                    &variant.ident.to_string().to_lowercase(),
-                    variant.ident.span(),
-                );
-                let variant_name = &variant.ident;
-                quote! {
-                    if self.#field_name.get().copied().unwrap_or(false) {
-                        variants.push(Type::#variant_name);
-                    }
+        data_enum.variants.iter().map(|variant| {
+            let field_name = Ident::new(&variant.ident.to_string().to_lowercase(), variant.ident.span());
+            let variant_name = &variant.ident;
+            quote! {
+                if self.#field_name.get().copied().unwrap_or(false) {
+                    variants.push(Type::#variant_name);
                 }
-            })
-            .collect::<Vec<_>>()
+            }
+        }).collect::<Vec<_>>()
     } else {
         vec![]
     };
